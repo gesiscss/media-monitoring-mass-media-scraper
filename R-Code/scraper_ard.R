7ea40ec1c44264e7095bdaa2017fbd53b2141517
Ehnert <ehnertpp@gesis.org>
2018-04-03 13:22:14 +0200

    Code eingefügt

---
+++ R-Code/scraper_ard.R
@@ -0,0 +1,15 @@
+#!/usr/local/bin/Rscript
+setwd("/Users/ehnertpp/newspaper-scraping-germany")
+source("./R-Code/functions.R")
+agentur <- "ard"
+input <- paste0("./data/newspaper_articles_germany/", agentur, "/indices" )
+output <- paste0("./data/newspaper_articles_germany/", agentur, "/articles" )
+
+# source functions and list of rss-feeds 
+
+
+rss_feeds <- sapply(Filter(function(x) x$agency == agentur, rss_feeds), "[[", "url")
+
+# run scraper:
+scraper_headlines(folder = input, rss_feeds, agency = agentur)
+scraper_articles(folderInput = input, folderOutput = output, agency = agentur)
